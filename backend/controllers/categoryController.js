a0012ed6991d7a587cf429b3220798a40f7b0f40
AbdiFitaah <47349181+AbdiFitaah@users.noreply.github.com>
2026-08-01 04:21:50 -0400

    heloo

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,4 +1,4 @@
-import Category from "../Models/Category.js";
+import Category from "../Models/category.js";
 export const getAllCategories = async (req, res) => {
   try {
     const categories = await Category.find().sort({ createdAt: -1 });
