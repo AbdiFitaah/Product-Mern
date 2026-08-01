2988a5f81a4421c3cf9dd9d55bffde9c6fa901e0
AbdiFitaah <47349181+AbdiFitaah@users.noreply.github.com>
2026-08-01 04:02:59 -0400

    heloo

--- frontend/src/pages/dashboard/DashboardPage.jsx
+++ frontend/src/pages/dashboard/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import DashboardHeader from "@components/Dashboard/DashboardHeader";
+import DashboardHeader from "@components/dashboard/DashboardHeader";
 import ProductForm from "@components/Product/ProductForm";
 import ProductList from '@components/Product/ProductList';
 
